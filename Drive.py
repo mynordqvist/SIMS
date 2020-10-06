01d6effbe7477700e1617a6ee424cfbaebf63bb7
myno1600 <myno1600@student.miun.se>
2020-10-06 13:48:07 +0200

    function to avoid crash, My and Felix

--- Drive.py
+++ Drive.py
@@ -1,7 +1,8 @@
 from adafruit_motorkit import MotorKit
 import time
+import RPi.GPIO as GPIO
 
-def Start():
+def Start(): #drive forwards
     
     kit = MotorKit()
     kit.motor1.throttle = 0.5
@@ -9,7 +10,8 @@ def Start():
     kit.motor3.throttle = 0.5
     kit.motor4.throttle = 0.5
 
-def Stop():
+def Stop(): #stop 
+
     kit = MotorKit()
     kit.motor1.throttle = 0
     kit.motor2.throttle = 0
@@ -17,18 +19,40 @@ def Stop():
     kit.motor4.throttle = 0
     time.sleep(2)
     
-def Turn():
+def Turn(): #turn right
+
     kit = MotorKit()
     kit.motor1.throttle = 0.8
     kit.motor3.throttle = 0.8
     kit.motor2.throttle = -0.8
     kit.motor4.throttle = -0.8
-    time.sleep(3)
-    kit.motor1.throttle = 0
-    kit.motor4.throttle = 0
-    kit.motor2.throttle = 0
-    kit.motor4.throttle = 0
 
+def Drive():
+    
+    GPIO.setwarnings(False) #disable warnings
+
+    #set ports
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(23, GPIO.IN)
+    GPIO.setup(24, GPIO.IN)
+    GPIO.setup(25, GPIO.IN)
+
+    while True:
+        
+        i = GPIO.input(23) 
+        k = GPIO.input(24)
+        j = GPIO.input(25)
+
+        #if any sensor detect an object 
+        if (i == 0 or j == 0 or k == 0):
+            Stop()
+            Turn()
+        
+        #if none of the sensors detects an object    
+        elif (i == 1 and j == 1 and k == 1):
+            Start()
+            
+    GPIO.cleanup() # clean up the ports   
 
     
 
