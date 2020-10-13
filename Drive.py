96fb90b10c90fd94211cd3d7242577099fdd562e
unknown <felixnystrom97@hotmail.com>
2020-10-13 15:51:02 +0200

    skapat sensor_init för avståndsgivarna, samt flyttat Run funktionen till en egen fil

--- Drive.py
+++ Drive.py
@@ -2,16 +2,17 @@ from adafruit_motorkit import MotorKit
 import time
 import RPi.GPIO as GPIO
 
-def Start(): #drive forwards
+#Drive forwards
+def Start():
     
     kit = MotorKit()
-    kit.motor1.throttle = 0.5
-    kit.motor2.throttle = 0.5
-    kit.motor3.throttle = 0.5
-    kit.motor4.throttle = 0.5
+    kit.motor1.throttle = 0.8
+    kit.motor2.throttle = 0.8
+    kit.motor3.throttle = 0.8
+    kit.motor4.throttle = 0.8
 
-def Stop(): #stop 
-
+#Stop
+def Stop():
     kit = MotorKit()
     kit.motor1.throttle = 0
     kit.motor2.throttle = 0
@@ -19,42 +20,24 @@ def Stop(): #stop
     kit.motor4.throttle = 0
     time.sleep(2)
     
-def Turn(): #turn right
-
+#Turn right    
+def Turn():
     kit = MotorKit()
     kit.motor1.throttle = 0.8
     kit.motor3.throttle = 0.8
     kit.motor2.throttle = -0.8
     kit.motor4.throttle = -0.8
-
-def Drive():
+    time.sleep(0.5)
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
+    kit.motor3.throttle = 0
+    kit.motor4.throttle = 0
     
-    GPIO.setwarnings(False) #disable warnings
+def Sensor_init():
+    GPIO.setwarnings(False) #Disable warnings
 
-    #set ports
+    #Set ports
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(23, GPIO.IN)
     GPIO.setup(24, GPIO.IN)
     GPIO.setup(25, GPIO.IN)
-
-    while True:
-        
-        i = GPIO.input(23) 
-        k = GPIO.input(24)
-        j = GPIO.input(25)
-
-        #if any sensor detect an object 
-        if (i == 0 or j == 0 or k == 0):
-            Stop()
-            Turn()
-        
-        #if none of the sensors detects an object    
-        elif (i == 1 and j == 1 and k == 1):
-            Start()
-            
-    GPIO.cleanup() # clean up the ports   
-
-    
-
-
-    
