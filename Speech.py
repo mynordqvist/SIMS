a3ae8bfb95c1cf448998efb913ec76115a279dfb
van975 <van19971014@gmail.com>
2020-10-21 21:17:45 +0200

    Added additional response and a wikipedia search function

--- Speech.py
+++ Speech.py
@@ -7,6 +7,10 @@ from datetime import datetime
 from gtts import gTTS as tts
 from Environment import *
 
+#need to install
+import wikipedia #pip install wikipedia
+import pytz #pip install pytz
+
 #Capture audio, input arg will be used to repeat question
 def Capture(str):
 
@@ -52,20 +56,75 @@ def Speak(text):
     
     #Remove file     
     os.remove(speech_file)
+    
+def Current_stockholm_time():
+    #set timezone
+    timezone = 'Europe/Stockholm'
+    local_tz = pytz.timezone(timezone)
+    
+    #get current time
+    now = datetime.datetime.now(pytz.utc)
+    
+    local_dt = now.replace(tzinfo=pytz.utc).astimezone(local_tz)
+    now_local = local_tz.normalize(local_dt)
+    
+    return now_local.hour
+
+def Greeting(currtime):
+    #Greeting alternative
+    if random.randint(0, 2) == 1:
+        #First get name
+        Speak('Hej,Vad heter du?')
+        name = Capture('Ursäkta jag hörde inte, kan du säga ditt namn igen?')
+        Speak('Hej, ' + str(name) + '.')
+        
+    else:
+        if currtime < 12:
+            Speak("Goddag")
+        elif currtime < 18:
+            Speak("God Eftermiddag")
+        else:
+            Speak("GodKväll")
+
+def Goodbye(currtime):
+    #Check time before 12
+    if currtime < 12:
+        lunch = 12 - currtime
+        if lunch > 1 and lunch <= 3:
+            Speak("Det är snart lunch, kämpa på")
+        else:
+            Speak("Ha en trevlig morgon")
+    
+    elif currtime == 12:
+        Speak("Ha en trevlig lunch")
+    
+    #Check time after 12        
+    elif currtime < 16:
+        slut = 16 - currtime
+        if slut > 1 and lunch <= 3:
+            Speak("Det är snart slut för idag, kämpa på")
+        else:
+            Speak("Ha en trevlig eftermiddag")
+    
+    else:
+        Speak("Ha en trevlig kväll")
 
 #Conversation with the user
 def Conversation():
     
-    #First get name
-    Speak('Hej,Vad heter du?')
-    name = Capture('Ursäkta jag hörde inte, kan du säga ditt namn igen?')
-    Speak('Hej, ' + str(name) + '.')
-
+    Greeting(Current_stockholm_time())
+    
     #Ask how the user is feeling
     greeting_phrases=['Hur mår du?','Mår du bra idag?']
     choosen_phrase = random.choice(greeting_phrases)
-    Speak(choosen_phrase+' '+name)
+    Speak(choosen_phrase)
     captured_text = Capture('Ursäkta jag hörde inte, mår du bra idag?').lower()
+    
+    if 'bra' in str(captured_text) or 'ja' in str(captured_text) or ' kanon' in str(captured_text) or 'fint' in str(captured_text):
+            Speak("Det är bra att veta att du mår bra")
+            
+    elif 'dålig' in str(captured_text) or 'nej' in str(captured_text) or 'ont' in str(captured_text):
+            Speak("Det var dålig att höra")
     
     #Then just keep listening & responding
     while 1:
@@ -80,10 +139,10 @@ def Conversation():
             Speak(d1)
             
         elif 'hjälp' in str(captured_text):
-            Speak('Tillgängliga funktioner: hälsning, datum, temperatur, luftfuktighet, hejdå och hjälp')
+            Speak('Det som jag kan hjälpa dig med: datum, temperatur, luftfuktighet, identifera en känd person/sak eller avsluta konvorsationen med att säga hejdå')
 
         elif 'hejdå' in str(captured_text):
-            Speak('Okej, hejdå ' + name + ', ha en trevlig dag.')
+            Goodbye(Current_stockholm_time())
             return
         
         elif 'temperatur' in str(captured_text):
@@ -95,6 +154,14 @@ def Conversation():
             humidity = Humidity()
             humidity = str(humidity)
             Speak('Luftfuktigheten är ' + humidity + ' %')
+            
+        elif 'vem är' in str(captured_text) or 'vad är'in str(captured_text): 
+            Speak('Söker Wikipedia') 
+            wikipedia.set_lang("sv")
+            results = wikipedia.summary(str(captured_text), sentences = 1) 
+            Speak("Enligt Wikipedia") 
+            Speak(results) 
         
         else:
-            Speak('Säg hjälp för att få tillgängliga funktioner')
+            Speak('Säg hjälp för att få reda på vad jag kan hjälpa med')
+
