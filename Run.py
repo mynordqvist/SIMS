3abf92bd510d5b068652b1e0a44b110514e22c38
unknown <felixnystrom97@hotmail.com>
2020-10-22 12:53:44 +0200

    Created threds for face detection and crash detection, reverse and then random function for turning left or right when crash or detect object in front of it, My and Felix

--- Run.py
+++ Run.py
@@ -1,14 +1,22 @@
 from Speech import *
 from Drive import *
 from FaceDetection import *
-import keyboard
-import sys
+from Accelerometer import *
+import threading
+import globals
+
 
 #Drive around and avoid crashes, find faces and start conversations 
 def Run():
     
     #Initialization of GPIO pins for sensors
     Sensor_init()
+    
+    #Threds for face detection and crash detection
+    faceThread = threading.Thread(target=FaceDetect)
+    crashThred = threading.Thread(target=Crash)
+    faceThread.start()
+    crashThred.start()
 
     while True:
         
@@ -16,22 +24,31 @@ def Run():
         k = GPIO.input(24)
         j = GPIO.input(25)
         
-        #If any sensor detect an object, stop and turn  
+        #If any sensor detect an object, stop and reverse then turn random left or right  
         if (i == 0 or j == 0 or k == 0):
             Stop()
-            Turn()
+            Reverse()
+            random_functions=[TurnLeft, TurnRight]
+            random.choice(random_functions)()
             
         #Otherwise continue driving 
         else:
             Start()
-            
-        face_found = FaceDetect() #Search for face
         
+        #If a crash is detected, stop and reverse then turn random left or right 
+        if globals.crash == True:
+            print("Crash")
+            Stop()
+            Reverse()
+            random_functions=[TurnLeft, TurnRight]
+            random.choice(random_functions)()
+            globals.crash = False #Reset global variable for crash is detected
+
         #If a face is found then stop and start a conversation
-        if face_found == True:
+        if globals.face_found == True:
             print('hej')
             Stop()
             Conversation()
-            face_found = False
+            globals.face_found = False #Reset global variable for face is found
         
     GPIO.cleanup() # Clean up the ports
