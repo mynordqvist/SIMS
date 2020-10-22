061f694f611f101b9c788cba80ce552c47bf5636
unknown <felixnystrom97@hotmail.com>
2020-10-22 12:48:38 +0200

    change parameters and set a global variable to true when a crash is detected

--- Accelerometer.py
+++ Accelerometer.py
@@ -2,15 +2,19 @@ import time
 import board
 import busio
 import adafruit_adxl34x
+import globals
 
+#Detects crashes
 def Crash():
     
+    #Initialization of accelerometer
     i2c = busio.I2C(board.SCL, board.SDA)
     accelerometer = adafruit_adxl34x.ADXL345(i2c)
-    accelerometer.enable_tap_detection(tap_count=1,threshold=65, duration=50)
-
+    accelerometer.enable_tap_detection(tap_count=1,threshold=100, duration=150)
+    
     while True:
-        if accelerometer.events["tap"] == True :
-            print("Crash")
-            return True
+        
+        #If accelerometer detects a crash, then set a global variable to True
+        if accelerometer.events["tap"] == True: 
+            globals.crash = True
 
