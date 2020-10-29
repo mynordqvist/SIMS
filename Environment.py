4426957e34ca3ae61a17432492bd1470b81f4b80
myno1600 <myno1600@student.miun.se>
2020-10-29 15:53:33 +0100

    Added function for reading co2 value and return different phrases depending on the values, My and Felix

--- Environment.py
+++ Environment.py
@@ -1,4 +1,7 @@
 import Adafruit_DHT
+import qwiic_ccs811
+import time
+import globals
 
 #Read humidity and temperature values from sensor
 def Read_sensor():
@@ -22,4 +25,39 @@ def Humidity():
     humidity, temperature = Read_sensor()
     
     return int(humidity)
+
+#Read the co2 value and update global variable
+def Co2Variable():
+    ccs = qwiic_ccs811.QwiicCcs811()
+    ccs.begin()
+    while True:
+        ccs.read_algorithm_results() #updates the TVOC and CO2 values
+        tvoc = ccs.get_tvoc()
+        co2 = ccs.get_co2()
+        time.sleep(5)
+        globals.co2 = co2
+
+#Return specific phrase depending on co2 value
+def Co2():
+
+    co2 = globals.co2
     
+    if co2 < 600:
+        return "Väldigt bra"
+    
+    elif co2 <800 and co2 >=600:
+        return "Bra"
+    
+    elif co2 <1000 and co2 >=800:
+        return "Ganska dålig, vädra gärna"
+        
+    
+    elif co2 <2000 and co2 >=1000:
+        return "Dålig, ni måste vädra! Annars kan trötthet och huvudvärk uppstå"
+    
+    elif co2 <=6000 and co2 >=2000:
+        return "Väldigt dålig, detta är maximala gränsen enligt Arbetsmiljöverket. Nu måste ni vädra!"
+        
+    
+    elif co2 > 6000:
+        return "Extremt dålig, ni får inte vistas i denna luft längre än 15 minuter!"  
